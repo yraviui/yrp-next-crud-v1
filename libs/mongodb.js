0a06cd81f8dec6dc79d51d8ff3ebcb9ce75b8485
unknown <yravi.ui@gmail.com>
2023-11-13 13:37:13 +0530

    update

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect('mongodb+srv://yravi:yrp12345@cluster0.xau9o.mongodb.net/p02-yrp-crud-db')
         console.log('MongoDB Connected')
     } catch (err) {
         console.log(err)
